44fc38e49bed52d8eedecad47093554b322350cb
mxBJJ <maxmendes0509@gmail.com>
2019-05-14 13:33:28 -0300

    primeiro commit

--- PrimeiroApp/ViewController.swift
+++ PrimeiroApp/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idade: UITextField!
+    
+    @IBOutlet weak var resposta: UILabel!
+    
+    @IBAction func descobrir(_ sender: Any) {
+        
+        if idade.text!.isEmpty {
+            resposta.textColor = UIColor.red
+            resposta.text = "É necessário informar uma idade para o cálculo."
+        }else{
+            
+    let idadeDog = Int(idade.text!)! * 7
+    resposta.textColor = UIColor.black
+    resposta.text = "O cachorro tem " + String(idadeDog) + " anos de idade em anos humanos."
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Meu primeiro app.")
     }
 
 
